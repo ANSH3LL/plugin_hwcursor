44cc80d8256bb321909d27c0828a0accce321eef
Ansh3ll <ansh3ll@protonmail.com>
2021-09-17 15:08:22 +0300

    Visual enhancements

--- Corona/main.lua
+++ Corona/main.lua
@@ -6,50 +6,53 @@ cursor.initPlugin()
 
 widget.newButton(
     {
-        width = 70,
+        width = 200,
+        height = 30,
+        shape = 'roundedRect',
+        x = display.contentCenterX,
+        y = display.contentCenterY - 100,
+        label = 'Reset to system cursor',
+        onRelease = function() cursor.resetCursor() end,
+        labelColor = { default = { 1, 1, 1 }, over = { 0, 0, 0, 0.5 } },
+        fillColor = { default = { 1, 0.2, 0.5, 0.7 }, over = { 1, 0.2, 0.5, 1 } }
+    }
+)
+
+widget.newButton(
+    {
+        width = 200,
         height = 30,
         shape = 'roundedRect',
         x = display.contentCenterX,
         y = display.contentCenterY - 50,
-        label = 'Reset to system cursor',
-        onRelease = function() cursor.resetCursor() end,
-        fillColor = { default = { 1, 0.2, 0.5, 0.7 }, over = { 1, 0.2, 0.5, 1 } }
-    }
-)
-
-widget.newButton(
-    {
-        width = 70,
-        height = 30,
-        shape = 'roundedRect',
-        x = display.contentCenterX,
-        y = display.contentCenterY - 25,
         label = 'Custom cursor 1',
         onRelease = function() cursor.loadCursor(system.pathForFile('cursor1.cur')) end,
+        labelColor = { default = { 1, 1, 1 }, over = { 0, 0, 0, 0.5 } },
         fillColor = { default = { 1, 0.2, 0.5, 0.7 }, over = { 1, 0.2, 0.5, 1 } }
     }
 )
 
 widget.newButton(
     {
-        width = 70,
+        width = 200,
         height = 30,
         shape = 'roundedRect',
         x = display.contentCenterX,
         y = display.contentCenterY,
         label = 'Custom cursor 2',
         onRelease = function() cursor.loadCursor(system.pathForFile('cursor2.ani')) end,
+        labelColor = { default = { 1, 1, 1 }, over = { 0, 0, 0, 0.5 } },
         fillColor = { default = { 1, 0.2, 0.5, 0.7 }, over = { 1, 0.2, 0.5, 1 } }
     }
 )
 
 widget.newButton(
     {
-        width = 70,
+        width = 200,
         height = 30,
         shape = 'roundedRect',
         x = display.contentCenterX,
-        y = display.contentCenterY + 25,
+        y = display.contentCenterY + 50,
         label = 'Show/Hide cursor',
         onRelease = function()
             if hidden then
@@ -59,23 +62,25 @@ widget.newButton(
             end
             hidden = not hidden
         end,
+        labelColor = { default = { 1, 1, 1 }, over = { 0, 0, 0, 0.5 } },
         fillColor = { default = { 1, 0.2, 0.5, 0.7 }, over = { 1, 0.2, 0.5, 1 } }
     }
 )
 
 widget.newButton(
     {
-        width = 70,
+        width = 200,
         height = 30,
         shape = 'roundedRect',
         x = display.contentCenterX,
-        y = display.contentCenterY + 50,
+        y = display.contentCenterY + 100,
         label = 'Exit application',
         onRelease = function()
             cursor.resetCursor()
             cursor.freePlugin()
             timer.performWithDelay(500, function() native.requestExit() end)
         end,
+        labelColor = { default = { 1, 1, 1 }, over = { 0, 0, 0, 0.5 } },
         fillColor = { default = { 1, 0.2, 0.5, 0.7 }, over = { 1, 0.2, 0.5, 1 } }
     }
 )
